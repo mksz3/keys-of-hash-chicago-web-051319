34f5e16385825d20f6a81c41973a0d4458c44bb1
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 19:26:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    # output = []
+    # self.each { |key, value| 
+    #   if args.include?(value)
+    #     output << key
+    #   end
+    # }
+    # output
+    output = self.select { |key, value| 
+      args.include?(value)
+    }
   end
 end
